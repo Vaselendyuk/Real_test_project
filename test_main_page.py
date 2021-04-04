d3dbbcf15a7edac99f8a7884b3f398f6cfc228f8
vaselendyuk <vaselendyuk@gmail.com>
2021-04-04 14:54:55 +0300

    Assert added

--- test_main_page.py
+++ test_main_page.py
@@ -17,3 +17,11 @@ def go_to_login_page(browser):
 def test_guest_can_go_to_login_page(browser):
    browser.get(link)
    go_to_login_page(browser)
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
