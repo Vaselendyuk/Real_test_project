ecff8f0c68a8e61a30e423b324051e873d91350b
vaselendyuk <vaselendyuk@gmail.com>
2021-04-04 13:59:02 +0300

    Update to Page Odject

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,19 @@
+from .pages.main_page import MainPage
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    page = MainPage(browser, link)
+    page = MainPage(browser, link)
+    page.go_to_login_page()
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
+def test_guest_can_go_to_login_page(browser):
+   browser.get(link)
+   go_to_login_page(browser)
